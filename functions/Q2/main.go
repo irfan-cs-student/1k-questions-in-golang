b178e9aaab5040986bfec41300ea007a30cddc9c
M-irfan <fannee0011@gmail.com>
2026-07-28 08:18:49 +0500

    2nd questions  solved

--- functions/Q2/main.go
+++ functions/Q2/main.go
@@ -1,1 +1,25 @@
-package q2
+package main
+
+import "fmt"
+
+//Write a function to print numbers from N to 1.
+
+func printNumbers(n int) {
+
+	if n >= 1 {
+
+		for a := n; a >= 1; a-- {
+			fmt.Print(a, " ")
+		}
+	} else {
+		for a := n; a <= 1; a++ {
+			fmt.Print(a, " ")
+		}
+	}
+}
+func main() {
+	var n int
+	fmt.Print("enter number n: ")
+	fmt.Scan(&n)
+	printNumbers(n)
+}
