a8631ff4b86f3b884f9f287928986c9729b2dd6c
M-irfan <fannee0011@gmail.com>
2026-07-28 08:25:32 +0500

    4th question solved

---
+++ functions/Q4/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// Write a function to print odd numbers from 1 to N.
+
+func printNumbers(n int) {
+	for a := 1; a <= n; a++ {
+		if a%2 != 0 {
+			fmt.Print(a, " ")
+		}
+	}
+}
+func main() {
+	var n int
+	fmt.Print("enter number n: ")
+	fmt.Scan(&n)
+	printNumbers(n)
+}
