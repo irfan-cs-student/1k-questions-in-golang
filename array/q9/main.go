e2f127912980369ab5d09beeba895ff909feeecc
M-irfan <fannee0011@gmail.com>
2026-07-31 08:12:57 +0500

    Find the average

---
+++ array/q9/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// Find the average
+func average(a, b int) int {
+
+	return a / b
+}
+
+func main() {
+
+	nums := [5]int{10, 20, 30, 40, 50}
+
+	sum := 0
+
+	for _, value := range nums {
+
+		sum += value
+	}
+	fmt.Println("averagr : ", average(sum, len(nums)))
+}
