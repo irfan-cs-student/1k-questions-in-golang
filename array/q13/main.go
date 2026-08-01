3149b7d0129a4225fb21c610e0836a56c28c1f7d
M-irfan <fannee0011@gmail.com>
2026-08-01 10:02:12 +0500

    printt duplicates value in array

--- array/q13/main.go
+++ array/q13/main.go
@@ -4,19 +4,62 @@ import "fmt"
 
 //Find duplicate values
 
-func average(a, b int) int {
-	return a / b
+func duplicate(array [9]int) {
+
+	for a := 0; a <= len(array)-1; a++ {
+
+		for b := a + 1; b <= len(array)-1; b++ {
+
+			if array[b] == array[a] {
+				fmt.Print(array[a], " ")
+			}
+		}
+	}
+
+}
+func dupElement(a [9]int, value int, size int) bool {
+
+	for i := 0; i < size; i++ {
+
+		if a[i] == value {
+			return true
+		}
+
+	}
+	return false
+}
+func findduplicate(array [9]int) [9]int {
+
+	index := 0
+	var result [9]int
+	for a := 0; a <= len(array)-1; a++ {
+
+		for b := a + 1; b <= len(array)-1; b++ {
+
+			if array[b] == array[a] {
+
+				value := array[b]
+				isRepeate := dupElement(result, value, index)
+
+				if isRepeate == false {
+					result[index] = array[b]
+					index++
+
+				}
+
+			}
+		}
+	}
+	return result
+
 }
 
 func main() {
 
-	nums := [5]int{10, 20, 30, 40, 50}
+	nums := [9]int{10, 20, 30, 40, 50, 10, 20, 20, 50}
+	fmt.Print("Duplicate values: ")
+	duplicate(nums)
+	fmt.Println()
+	fmt.Print("Duplicaten: ", findduplicate(nums))
 
-	sum := 0
-
-	for _, value := range nums {
-
-		sum += value
-	}
-	fmt.Println("averagr : ", average(sum, len(nums)))
 }
