54db91e2cc49e301b17a0656482983c5835c4ef6
M-irfan <fannee0011@gmail.com>
2026-08-06 16:20:45 +0500

    -2nd and -3rd smallest elements in array

---
+++ array/q34/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Return smallest, second smallesr number, 3rd smallest and their diference
+
+func extremeNum(a [6]int) (smallest, _2ndSmall, _3rdSmall int) {
+
+	smallest, _2ndSmall, _3rdSmall = math.MaxInt, math.MaxInt, math.MaxInt
+
+	for _, value := range a {
+
+		if value < smallest {
+			_3rdSmall = _2ndSmall
+			_2ndSmall = smallest
+			smallest = value
+
+		} else if value > smallest && value < _2ndSmall {
+
+			_3rdSmall = _2ndSmall
+			_2ndSmall = value
+		} else if value < _3rdSmall {
+			_3rdSmall = value
+		}
+
+	}
+
+	return
+}
+
+func main() {
+
+	nums := [6]int{2, 1, -7, 0, 4, -2}
+
+	smallest, _2ndSmall, _3rdSmall := extremeNum(nums)
+	fmt.Println("smallest:", smallest)
+	fmt.Println("2ndsmall:", _2ndSmall)
+	fmt.Println("3rdsmall:", _3rdSmall)
+
+}
