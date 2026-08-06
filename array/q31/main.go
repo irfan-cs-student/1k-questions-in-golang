b26927dc32c1a0c9610848f3f575b9465cbe0939
M-irfan <fannee0011@gmail.com>
2026-08-06 12:10:53 +0500

    Reverse an array without creating another array.

---
+++ array/q31/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// Reverse an array without creating another array.
+
+func reverseArray(a [6]int) [6]int {
+
+	index := len(a) - 1
+	center := len(a) / 2
+
+	for i := 0; i < len(a); i++ {
+
+		if i < center {
+			temp := a[i]
+			a[i] = a[index]
+			a[index] = temp
+			index--
+		}
+
+	}
+
+	return a
+}
+
+func main() {
+
+	nums := [6]int{2, 1, 7, 0, 4, -2}
+
+	fmt.Print("pairs array:", reverseArray(nums))
+}
