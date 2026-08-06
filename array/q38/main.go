002ce60339aa77efc73da387ff50e4850d311366
M-irfan <fannee0011@gmail.com>
2026-08-06 17:58:04 +0500

    rotating elements in array

--- array/q38/main.go
+++ array/q38/main.go
@@ -2,26 +2,54 @@ package main
 
 import "fmt"
 
-// Rotate array to the left by 'steps'
+// Rotate array one position to the left
+func moveElement(a [6]int) (result [6]int) {
+
+	temp := a[0]
+
+	for index := range a {
+
+		if index == len(a)-1 {
+			result[index] = temp
+		} else {
+			result[index] = a[index+1]
+		}
+	}
+	return
+}
+
+// Rotate array to the left by any number of steps
 func moveAhead(a [6]int, steps int) (result [6]int) {
 
-	steps = steps % len(a)
-	// Handle steps greater than array length
+	steps = steps % len(a) // Handle steps larger than array size
 
-	for i := range a {
-		result[i] = a[(i+steps)%len(a)]
+	tempRange := steps
+
+	for index := range a {
+
+		if tempRange < len(a) {
+			result[index] = a[tempRange]
+		} else {
+			result[index] = a[tempRange-len(a)]
+		}
+
+		tempRange++
 	}
 
 	return
 }
 
 func main() {
+
 	num := [6]int{1, 2, 3, 4, 5, 6}
+
+	fmt.Println("Original array:", num)
+	fmt.Println("Move 1 step:", moveElement(num))
 
 	var steps int
 	fmt.Print("Enter steps: ")
 	fmt.Scan(&steps)
 
-	fmt.Println("Original:", num)
-	fmt.Println("Rotated :", moveAhead(num, steps))
+	fmt.Println("Original array:", num)
+	fmt.Println("Move", steps, "steps:", moveAhead(num, steps))
 }
