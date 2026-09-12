586ea938d0d0dde30e90e1713c54d5e7ea7f2bea
M-irfan <fannee0011@gmail.com>
2026-09-12 12:32:09 +0500

    question 3 started

---
+++ maps/q3/main.go
@@ -0,0 +1,13 @@
+// 2. First Unique Character
+
+// Given:
+
+// word := "programming"
+
+// Find the first character that appears only once.
+
+// Expected output:
+
+// First unique character: p
+
+// Logic challenge: Count all characters first, then make a second pass to find the first count of 1
